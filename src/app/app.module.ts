035956a681b1596a2f30fd888ec1515f948f498d
Kirk Byers <kbyers@ljaengineering.com>
2017-04-27 15:54:20 -0500

    Add Material library.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -10,6 +12,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
     HttpModule
