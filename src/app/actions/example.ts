fe7e88d5dc614033dabeed2e030ce55a90be1d9d
Kirk Byers <kbyers@ljaengineering.com>
2017-05-01 10:14:52 -0500

    Example action.

---
+++ src/app/actions/example.ts
@@ -0,0 +1,22 @@
+import { Action } from '@ngrx/store';
+
+import { type } from '../utils';
+
+export const ActionTypes = {
+  EXAMPLE_UPDATE:     type('[Example] Update'),
+  EXAMPLE_SET:        type('[Example] Set')
+};
+
+export class UpdateAction implements Action {
+  type = ActionTypes.EXAMPLE_UPDATE;
+  constructor (public payload: any) {}
+}
+
+export class SetAction implements Action {
+  type = ActionTypes.EXAMPLE_SET;
+  constructor (public payload: any) {}
+}
+
+export type Actions
+  = UpdateAction
+  | SetAction;
